118e20dbf2e314dbe6dfbc4bc92bc32ebfd1ece7
JAVOHIR <javohirochilov2013@gmail.com>
2025-09-18 10:18:57 +0500

    Add files via upload

---
+++ ish/script.js
@@ -0,0 +1,9 @@
+let yosh = prompt("yoshingizni kiriting: ");
+if (yosh > 15) {
+    let login = prompt("login kiriting");
+    let parol = prompt("parol kiriting");
+    if ((login == "salom") == (parol == 1234))_{
+        let savol = prompt("99+99=?")
+        if(savol == 188)
+    }
+ 
